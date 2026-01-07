c3189b76e018a5a20fffe7e843aec19bbf082fb7
Balakrishnan J <j.balakrishnan2005@gmail.com>
2026-01-07 12:23:24 +0530

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-// Get display element AFTER page loads
 const display = document.getElementById("display");
-
-/* Append number or operator */
 function appendValue(value) {
     display.value += value;
 }
-
-/* Clear entire display */
 function clearDisplay() {
     display.value = "";
 }
 
-/* Delete last character (DEL button) */
 function deleteLast() {
     if (display.value.length > 0) {
         display.value = display.value.slice(0, -1);
     }
 }
 
-/* Calculate result */
 function calculate() {
     try {
         if (display.value === "") return;
@@ -28,7 +21,6 @@ function calculate() {
     }
 }
 
-/* Square function */
 function square() {
     if (display.value === "") return;
 
